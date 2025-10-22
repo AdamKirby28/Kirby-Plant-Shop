7e44739a2596a9160ddebe933bd152ae07991fa5
AdamKirby28 <422000630@ntc.edu.ph>
2025-10-22 19:39:21 +0800

    Create plants.js

---
+++ src/plants.js
@@ -0,0 +1,46 @@
+const plants = [
+  {
+    id: 1,
+    name: "Kirby Rose",
+    category: "Flowering",
+    price: 12.99,
+    image: "/assets/images/plant1.jpg",
+  },
+  {
+    id: 2,
+    name: "Starleaf",
+    category: "Foliage",
+    price: 9.99,
+    image: "/assets/images/plant2.jpg",
+  },
+  {
+    id: 3,
+    name: "Dream Cactus",
+    category: "Succulent",
+    price: 7.99,
+    image: "/assets/images/plant3.jpg",
+  },
+  {
+    id: 4,
+    name: "Pink Puff",
+    category: "Flowering",
+    price: 14.99,
+    image: "/assets/images/plant4.jpg",
+  },
+  {
+    id: 5,
+    name: "Green Star",
+    category: "Foliage",
+    price: 11.49,
+    image: "/assets/images/plant5.jpg",
+  },
+  {
+    id: 6,
+    name: "Bubble Succulent",
+    category: "Succulent",
+    price: 8.49,
+    image: "/assets/images/plant6.jpg",
+  },
+];
+
+export default plants;
